2fb78521997cf3d0f8c87c5e03ef3d9a3e94dd4c
laurabluna <laurablunam@gmail.com>
2026-05-18 15:45:00 -0300

    :sparkles: feat: adiciona lint e erro proposital

--- FirstAPI/main.py
+++ FirstAPI/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 from FirstAPI.routers import api_router
+import sys
 
 app = FastAPI(title="Primeira construção de API")
 app.include_router(api_router)
