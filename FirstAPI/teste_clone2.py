2c0ccd490115bb5bb2eb9a53eae6b82891f925a9
laurabluna <laurablunam@gmail.com>
2026-05-31 17:30:25 -0300

    :sparkles: feat: adiciona duplicação proposital

---
+++ FirstAPI/teste_clone2.py
@@ -0,0 +1,16 @@
+def teste():
+    nome = "Laura"
+    idade = 21
+    cidade = "Natal"
+    curso = "ADS"
+
+    print(nome)
+    print(idade)
+    print(cidade)
+    print(curso)
+
+    for i in range(10):
+        print(i)
+
+    print("Fim da execução")
+    return nome
